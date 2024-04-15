29dec66d64b14068c5ba7c420d3804e43c2934a1
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-15 10:02:00 -0300

    Launch types

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,12 @@
+import { type Launch } from "@/types/Launch";
 const getApiUrl = async (path: string) => {
   const response = await fetch(`https://api.spacexdata.com/v5/${path}`);
   return response.json();
 };
 
-export async function getAllLaunches() {
+export async function getAllLaunches(): Promise<Launch[]> {
   return await getApiUrl("launches");
 }
-export async function getLaunchById(id: number) {
+export async function getLaunchById(id: number): Promise<Launch> {
   return await getApiUrl(`launches/${id}`);
 }
---
+++ src/types/Launch.d.ts
@@ -0,0 +1,75 @@
+export interface Launch {
+  fairings: Fairings;
+  links: Links;
+  static_fire_date_utc: string;
+  static_fire_date_unix: number;
+  net: boolean;
+  window: number;
+  rocket: string;
+  success: boolean;
+  failures: any[];
+  details: string;
+  crew: any[];
+  ships: string[];
+  capsules: any[];
+  payloads: string[];
+  launchpad: string;
+  flight_number: number;
+  name: string;
+  date_utc: string;
+  date_unix: number;
+  date_local: string;
+  date_precision: string;
+  upcoming: boolean;
+  cores: Core[];
+  auto_update: boolean;
+  tbd: boolean;
+  launch_library_id: any;
+  id: string;
+}
+
+interface Fairings {
+  reused: any;
+  recovery_attempt: boolean;
+  recovered: boolean;
+  ships: string[];
+}
+
+interface Links {
+  patch: Patch;
+  reddit: Reddit;
+  flickr: Flickr;
+  presskit: any;
+  webcast: string;
+  youtube_id: string;
+  article: string;
+  wikipedia: string;
+}
+
+interface Patch {
+  small: string;
+  large: string;
+}
+
+interface Reddit {
+  campaign: string;
+  launch: string;
+  media: string;
+  recovery: string;
+}
+interface Flickr {
+  small: any[];
+  original: string[];
+}
+
+interface Core {
+  core: string;
+  flight: number;
+  gridfins: boolean;
+  legs: boolean;
+  reused: boolean;
+  landing_attempt: boolean;
+  landing_success: boolean;
+  landing_type: string;
+  landpad: string;
+}
