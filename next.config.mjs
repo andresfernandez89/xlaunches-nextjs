6bde00cc0120542d43513b153dfdb194be724183
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-15 15:02:11 -0300

    config remotePatterns on nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images2.imgbox.com",
+        pathname: "**",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
