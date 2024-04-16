aa84af261e9684cf2eb94eff8cb828987af2ab75
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-16 10:36:03 -0300

    lauchCard component

---
+++ src/app/components/launchCard/launchCard.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { type Launch } from "@/types/Launch";
+export default async function LaunchCard({
+  flightNumber,
+  status,
+  name,
+  date,
+  image,
+}) {
+  const dateFormatted = date.substring(0, 10);
+  return (
+    <Card>
+      <CardHeader>
+        <Badge className="pointer-events-none w-fit text-sm">
+          #{flightNumber}
+        </Badge>
+        <Badge
+          className={`pointer-events-none w-fit text-xs font-light text-white ${status ? "bg-green-700" : "bg-red-800"}`}
+        >
+          {status ? "Successful" : "Failed"}
+        </Badge>
+        <CardTitle className="text-md">{name}</CardTitle>
+        <CardDescription>{dateFormatted}</CardDescription>
+      </CardHeader>
+      <CardContent className="h-auto">
+        {image && (
+          <Image
+            className={"m-auto h-full"}
+            src={image}
+            width={140}
+            height={140}
+            alt={"foto"}
+          />
+        )}
+      </CardContent>
+    </Card>
+  );
+}
