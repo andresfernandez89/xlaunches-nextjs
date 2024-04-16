39a82e440cb3bfa42278108e2cbbc34522b202f5
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-16 15:33:28 -0300

    LaunchesContainer component

---
+++ src/app/components/launchesContainer/launchesContainer.tsx
@@ -0,0 +1,22 @@
+import { getAllLaunches } from "@/lib/api";
+import LaunchCard from "../launchCard/launchCard";
+import { Launch } from "@/types/LaunchApi";
+
+export default async function LaunchesContainer() {
+  const lauches = await getAllLaunches();
+  return (
+    <section className="my-10 grid grid-cols-cards gap-4 sm:gap-6">
+      {lauches.map((launch: Launch) => (
+        <LaunchCard
+          key={launch.id}
+          name={launch.name}
+          flightNumber={launch.flight_number}
+          status={launch.success}
+          image={launch.links.patch.small}
+          date={launch.date_utc}
+          webcast={launch.links.webcast}
+        />
+      ))}
+    </section>
+  );
+}
