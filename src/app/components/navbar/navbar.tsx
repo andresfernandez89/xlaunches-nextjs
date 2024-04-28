4eabd03e2175a7ad5969b4e0ca594e8d2f04f2eb
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-28 18:05:39 -0300

    fix errors

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import logo from "@/../public/SpaceX-Logo.svg";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex items-center justify-center max-sm:flex-col">
@@ -10,9 +10,14 @@ export default function Navbar() {
         width={275}
         height={150}
         alt="SpaceX logo"
-        className="h-24"
+        style={{
+          objectFit: "cover",
+          width: "275px",
+          height: "150px",
+        }}
+        priority
       />
-      <div className="flex gap-2 max-sm:mt-5 sm:ml-4">
+      <div className="flex gap-2 self-end max-sm:mt-5 sm:ml-4">
         <Button asChild>
           <Link href="/failed">Failed</Link>
         </Button>
