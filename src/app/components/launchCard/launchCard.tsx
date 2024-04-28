3b72c454f02ff36ec45b34bb714abee3e851d881
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-28 19:35:00 -0300

    default img launch

--- src/app/components/launchCard/launchCard.tsx
+++ src/app/components/launchCard/launchCard.tsx
@@ -1,3 +1,4 @@
+import logoDefault from "@/../public/default-img-launch.webp";
 import { Badge } from "@/components/ui/badge";
 import {
   Card,
@@ -33,10 +34,19 @@ export default function LaunchCard({
         <CardDescription>{dateFormatted}</CardDescription>
       </CardHeader>
       <CardContent className="h-auto self-end">
-        {image && (
+        {image ? (
           <Image
             className={"m-auto h-full"}
             src={image}
+            width={140}
+            height={140}
+            alt={`${name} launch`}
+            style={{ objectFit: "cover" }}
+          />
+        ) : (
+          <Image
+            className={"m-auto h-full"}
+            src={logoDefault}
             width={140}
             height={140}
             alt={`${name} launch`}
